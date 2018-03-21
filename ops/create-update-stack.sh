cc1e7f59defd991e8c98516a1475116e7650bdf9
James Cooper <jcooper@auraframes.com>
2018-03-21 15:27:04 -0700

    create-update-stack.sh: add param support

--- ops/create-update-stack.sh
+++ ops/create-update-stack.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-set -e
+stack_name="poc-cf-packer-asg"
 
-aws cloudformation create-stack \
+# default MaxClusterSize to be larger than ClusterSize
+# this allows rolling updates to start booting new servers
+# before stopping existing ones
+if [ -z "${ClusterSize}" ]; then
+    ClusterSize=1
+fi
+MaxClusterSize=$(($ClusterSize+4))
+
+# build parameters
+param_names=(Subnets VPC ClusterSize MaxClusterSize InstanceType AMI SSHKeyName AppVersion)
+params=""
+for p in "${param_names[@]}"
+do
+    val="${!p}"
+    if [ -n "${val}" ]; then
+        if [ "${p}" = "Subnets" ]; then
+            val="\"${val}\""
+        fi
+        params="${params} ParameterKey=${p},ParameterValue=${val}"
+    fi
+done
+
+# check if stack already exists
+action="create-stack"
+aws cloudformation describe-stacks --stack-name ${stack_name} > /dev/null
+if [ $? -eq 0 ]; then
+    action="update-stack"
+fi
+
+aws cloudformation ${action} \
   --stack-name poc-cf-packer-asg \
   --template-body file://./ops/codebuild-asg-stack.yml \
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+  --parameters ${params}
