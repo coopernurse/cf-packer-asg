3b6f2add6cf10277f1b1f391153497f0c8d28495
James Cooper <jcooper@auraframes.com>
2018-03-21 15:25:25 -0700

    create-update-stack.sh: rename from create-stack.sh, update to support stack params; add rolling-update-test.py

--- ops/update-stack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-aws cloudformation update-stack \
-  --stack-name poc-cf-packer-asg \
-  --template-body file://./ops/codebuild-asg-stack.yml \
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
